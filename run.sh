b3f19c99335fe62a7a2bbafb057652e95aad2fbd
Artem Prudishew <prudishew@gmail.com>
2022-05-06 11:06:19 +0300

    Fix .env and run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,9 @@ php bin/console make:migration
 php bin/console doctrine:migrations:migrate
 
 php bin/console cache:clear
-yarn run build
 
-##./vendor/bin/php-cs-fixer fix
+./vendor/bin/php-cs-fixer fix
+
 ##./vendor/bin/phpstan analyse src tests
+
+yarn run build

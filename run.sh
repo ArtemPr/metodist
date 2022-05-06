c958103b1f75f9397d7bfaa48d006f053b8ec103
Artem Prudishew <prudishew@gmail.com>
2022-05-06 09:06:39 +0300

    test

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 ## run in CI/CD
+
 rm -R ./vendor/*
 rm ./composer.lock
 composer install
+
 php bin/console make:migration
 php bin/console doctrine:migrations:migrate
+
 php bin/console cache:clear
 yarn build
+
 ./vendor/bin/php-cs-fixer fix
 ./vendor/bin/phpstan analyse src tests

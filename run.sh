39391037f09a0ae44b3c71e62da3deeb4785a8a3
Artem Prudishew <prudishew@gmail.com>
2022-05-11 09:35:34 +0300

    Add sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 ## Run in CI/CD
+ls -la
+##composer update
 
-composer update
+##php bin/console doctrine:migrations:migrate --no-interaction
+##php bin/console cache:clear
 
-php bin/console doctrine:migrations:migrate --no-interaction
-php bin/console cache:clear
-
-## yarn run build
+##yarn run build
 
 ## ./vendor/bin/php-cs-fixer fix

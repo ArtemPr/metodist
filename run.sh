15271a214d398779dee125690c60526e1f54fd70
Artem Prudishew <prudishew@gmail.com>
2022-05-11 09:36:25 +0300

    Edit sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 ## Run in CI/CD
-ls -la
-##composer update
 
-##php bin/console doctrine:migrations:migrate --no-interaction
-##php bin/console cache:clear
+composer update
+
+php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console cache:clear
 
 ##yarn run build
 
